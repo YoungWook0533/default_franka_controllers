6ae4e72533e6ce9961903eed6780c76d40a11931
YoungWook0533 <monglee147@gmail.com>
2025-09-26 12:23:27 +0900

    fixed launch file name

--- dyros_fr3_controllers/launch/start_control.launch.py
+++ dyros_fr3_controllers/launch/start_control.launch.py
@@ -1,50 +1,3 @@
-#  Copyright (c) 2025 Franka Robotics GmbH
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-############################################################################
-# Parameters:
-# controller_name: Name of the controller to spawn (required, no default)
-# robot_config_file: Path to the robot configuration file to load
-#                   (default: franka.config.yaml in franka_bringup/config)
-#
-# The example.launch.py launch file provides a flexible and unified interface
-# for launching Franka Robotics example controllers via the 'controller_name'
-# parameter, such as 'elbow_example_controller'.
-# Example:
-# ros2 launch franka_bringup example.launch.py controller_name:=elbow_example_controller
-#
-# This script "includes" franka.launch.py to declare core component nodes,
-# including: robot_state_publisher, ros2_control_node, joint_state_publisher,
-# joint_state_broadcaster, franka_robot_state_broadcaster, and optionally
-# franka_gripper and rviz, with support for namespaced and non-namespaced
-# environments as defined in franka.config.yaml. RViz is launched if
-# 'use_rviz' is set to true in the configuration file.
-#
-# The default robot_config_file is franka.config.yaml in the
-# franka_bringup/config directory. See that file for its own documentation.
-#
-# This approach improves upon the earlier individual launch scripts, which
-# varied in structure and lacked namespace support, offering a more consistent
-# and maintainable solution. While some may favor the older scripts for their
-# specific use cases, example.launch.py enhances scalability and ease of use
-# for a wide range of Franka Robotics applications.
-#
-# Ensure the specified  controller_name matches a controller defined in
-#  controllers.yaml to avoid runtime errors.
-############################################################################
-
-
 import os
 import sys
 from ament_index_python.packages import get_package_share_directory
@@ -60,14 +13,7 @@ package_share = get_package_share_directory('dyros_fr3_controllers')
 utils_path = os.path.join(package_share, '..', '..', 'lib', 'dyros_fr3_controllers', 'utils')
 sys.path.append(os.path.abspath(utils_path))
 
-from launch_utils import load_yaml  # noqa: E402
-
-# Iterates over the uncommented lines in file specified by the robot_config_file parameter.
-# "Includes" franka.launch.py for each active (uncommented) Robot.
-# That file is well documented.
-# The function also checks if the 'use_rviz' parameter is set to true in the YAML file.
-# If so, it includes a node for RViz to visualize the robot's state.
-# The function returns a list of nodes to be launched.
+from launch_utils import load_yaml
 
 
 def generate_robot_nodes(context):
